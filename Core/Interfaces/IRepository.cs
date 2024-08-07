8aaf671cfa0f27acbc8bb2f8c8353460942c1622
Ebraheem <ebraheem20012020@gmail.com>
2024-08-07 20:37:32 +0300

    interfaces for generic repository and unit of work

--- Core/Interfaces/IRepository.cs
+++ Core/Interfaces/IRepository.cs
@@ -1,4 +1,4 @@
-using Pharmacy.Domain.Models;
+using Pharmacy.Domain.Generics;
 
 namespace Pharmacy.Domain.Interfaces;
 
@@ -6,8 +6,8 @@ namespace Pharmacy.Domain.Interfaces;
 public interface IRepository<model_type> where model_type: BaseModel
 {
     IEnumerable<model_type> GetAll();
-    model_type GetById(int id);
-    model_type GetById(Guid id);
+    model_type? GetById(int id);
+    model_type? GetById(Guid id);
     model_type Add(model_type model);
     model_type Update(model_type model);
     void Remove(model_type model);
--- Core/Interfaces/IRepositoryManager.cs
+++ Core/Interfaces/IRepositoryManager.cs
@@ -1,18 +1,16 @@
-using Pharmacy.Domain.Models.ProductsModule;
-using Pharmacy.Domain.Models.OrdersModule;
-using Pharmacy.Domain.Models.UsersModule;
+using Pharmacy.Domain.Modules.Products.Models;
+using Pharmacy.Domain.Modules.Orders.Models;
 
 namespace Pharmacy.Domain.Interfaces;
 
 
-public interface IRepositoryManager: IDisposable
+public interface IRepositoryManager : IDisposable
 {
     IRepository<Product> Products {get; }
     IRepository<ProductProvider> ProductProviders {get; }
     IRepository<IncomingOrder> IncomingOrders {get; }
     IRepository<ProductItem> ProductItems {get; }
-    // IRepository<Customer> Customers {get; }
-    // IRepository<Order> Orders {get; }
-    // IRepository<ScarceProduct> ScarceProducts {get; }
-    // IRepository<User> Users {get; }
+    IRepository<Customer> Customers {get; }
+    IRepository<Order> Orders {get; }
+    IRepository<ScarceProduct> ScarceProducts {get; }
 }
