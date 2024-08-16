bc63eade1cd9214415b45504f626d8e1d2d25b26
Ebraheem <ebraheem20012020@gmail.com>
2024-08-16 21:03:40 +0300

    generalize search field

--- Shared/Generics/NotFoundResponse.cs
+++ Shared/Generics/NotFoundResponse.cs
@@ -5,6 +5,6 @@ namespace Pharmacy.Shared.Generics;
 public class NotFoundResponse : BaseResponse
 {
     public string Message;
-    public NotFoundResponse(object id, string resource) : base(false, 404) =>
-        Message = $"{resource} with id {id} Not Found";
+    public NotFoundResponse(object id, string resource, string idField = "Id") : base(false, 404) =>
+        Message = $"{resource} with {idField} {id} Not Found";
 }
