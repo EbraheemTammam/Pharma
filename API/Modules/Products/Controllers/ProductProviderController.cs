7d2c720bc5e7471078b449982078ccbcd631103f
Ebraheem <ebraheem20012020@gmail.com>
2024-08-16 12:57:17 +0300

    product provider endpoints

---
+++ API/Modules/Products/Controllers/ProductProviderController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Presentation.Extensions;
+using Pharmacy.Presentation.Generics;
+using Pharmacy.Services;
+using Pharmacy.Shared.Generics;
+using Pharmacy.Shared.Modules.Products.DTOs;
+
+namespace Pharmacy.Presentation.Controllers;
+
+
+[ApiController]
+public class ProductProvidersController : GenericController
+{
+    private readonly IProductProviderService _productProviderService;
+    public ProductProvidersController(IProductProviderService productProviderService) =>
+        _productProviderService = productProviderService;
+
+    [HttpGet]
+    public IActionResult Get() =>
+        Ok(
+            _productProviderService.GetAll()
+            .GetResult<IEnumerable<ProductProviderDTO>>()
+        );
+
+    [HttpGet("{id}")]
+    public IActionResult Get(Guid id)
+    {
+        BaseResponse response = _productProviderService.GetById(id);
+        if(!response.Success) ProcessError(response);
+        return Ok(response.GetResult<ProductProviderDTO>());
+    }
+
+    [HttpPost]
+    public IActionResult Create(ProductProviderCreateDTO productProvider) =>
+        Ok(
+            _productProviderService.Create(productProvider).GetResult<ProductProviderDTO>()
+        );
+
+    [HttpPut]
+    public IActionResult Update(Guid id, ProductProviderCreateDTO productProvider)
+    {
+        BaseResponse response = _productProviderService.Update(id, productProvider);
+        if(!response.Success) ProcessError(response);
+        return Ok(response.GetResult<ProductProviderDTO>());
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(Guid id)
+    {
+        BaseResponse response = _productProviderService.Remove(id);
+        if(!response.Success) ProcessError(response);
+        return Ok(response.GetResult<ProductProviderDTO>());
+    }
+}
