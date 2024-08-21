a5da203218bf426f29c89faf7644e8296ffbc261
Ebraheem <ebraheem20012020@gmail.com>
2024-08-21 05:58:35 +0300

    decimal to double

--- Core/Modules/Products/Models/IncomingOrder.cs
+++ Core/Modules/Products/Models/IncomingOrder.cs
@@ -5,10 +5,10 @@ namespace Pharmacy.Domain.Modules.Products.Models;
 
 public sealed class IncomingOrder : BaseModel<Guid>
 {
-    public decimal Price {get; set;}
-    public decimal Paid {get; set;}
+    public double Price {get; set;}
+    public double Paid {get; set;}
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
     public Guid ProviderId {get; set;}
     public ProductProvider? Provider {get; set;}
-    public IEnumerable<ProductItem>? Products {get; set;}
+    public IEnumerable<ProductItem>? Items {get; set;}
 }
