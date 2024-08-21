597216a1a8ad1fd1abeab5970a2e49a7739d126a
Ebraheem <ebraheem20012020@gmail.com>
2024-08-21 11:12:55 +0300

    generalize function instead of duplication

--- Service/Modules/Products/IProductService.cs
+++ Service/Modules/Products/IProductService.cs
@@ -7,9 +7,8 @@ namespace Pharmacy.Services;
 public interface IProductService
 {
     BaseResponse GetAll();
-    BaseResponse GetById(Guid id);
-    BaseResponse GetObjectById(Guid id);
-    BaseResponse GetObjectByBarcode(string barcode);
+    BaseResponse GetById(Guid id, bool AsDTO = true);
+    BaseResponse GetByBarcode(string barcode, bool AsDTO = true);
     BaseResponse Create(ProductCreateDTO schema);
     BaseResponse Update(Guid id, ProductCreateDTO schema);
     BaseResponse Delete(Guid id);
