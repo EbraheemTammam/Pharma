690fb837f27bc5e39e00ef6102720da1dea67113
Ebraheem <ebraheem20012020@gmail.com>
2024-08-21 05:57:32 +0300

    remove reverse property

--- Core/Modules/Products/Models/ProductProvider.cs
+++ Core/Modules/Products/Models/ProductProvider.cs
@@ -6,8 +6,4 @@ namespace Pharmacy.Domain.Modules.Products.Models;
 public sealed class ProductProvider : BaseModel<Guid>
 {
     public required string Name {get; set;}
-    public IEnumerable<IncomingOrder>? IncomingOrders {get; set;}
-    public decimal Indepted() =>
-        this.IncomingOrders is null ?
-        0 : this.IncomingOrders.Sum(order => order.Price - order.Paid);
 }
