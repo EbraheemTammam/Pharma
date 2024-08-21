74005871021d7eddae2b1fe3dd4ca7590d9e2402
Ebraheem <ebraheem20012020@gmail.com>
2024-08-21 05:54:04 +0300

    required fields only

--- Shared/Modules/Products/DTOs/IncomingOrderDTOs.cs
+++ Shared/Modules/Products/DTOs/IncomingOrderDTOs.cs
@@ -32,5 +32,5 @@ public record IncomingOrderDTO : IncomingOrderBaseDTO
 {
     public Guid Id {get; set;}
     public DateTime CreatedAt {get; set;}
-    public required ProductProviderDTO Provider {get; set;}
+    public required string ProviderName {get; set;}
 }
