1b91de8adbc8399c47a8d54a0b0c624939bbec3d
Ebraheem <ebraheem20012020@gmail.com>
2024-08-22 20:43:18 +0300

    correct config entity type

--- Infrastructure/Modules/Users/Data/Configurations/CustomUserConfig.cs
+++ Infrastructure/Modules/Users/Data/Configurations/CustomUserConfig.cs
@@ -1,13 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Pharmacy.Domain.Modules.Products.Models;
+using Pharmacy.Domain.Modules.Users.Models;
 
 namespace Pharmacy.Infrastructure.Modules.Products.Data.Configurations;
 
 
-public class CustomUserConfigs : IEntityTypeConfiguration<IncomingOrder>
+public class CustomUserConfigs : IEntityTypeConfiguration<CustomUser>
 {
-    public void Configure(EntityTypeBuilder<IncomingOrder> builder)
+    public void Configure(EntityTypeBuilder<CustomUser> builder)
     {
         builder.HasKey(user => user.Id);
     }
