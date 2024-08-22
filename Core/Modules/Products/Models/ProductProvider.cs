ee7dab4ce7d041136ad759e48424701ec3d28cbe
Ebraheem <ebraheem20012020@gmail.com>
2024-08-22 20:45:06 +0300

    add indepted field

--- Core/Modules/Products/Models/ProductProvider.cs
+++ Core/Modules/Products/Models/ProductProvider.cs
@@ -6,4 +6,5 @@ namespace Pharmacy.Domain.Modules.Products.Models;
 public sealed class ProductProvider : BaseModel<Guid>
 {
     public required string Name {get; set;}
+    public double Indepted {get; set;}
 }
