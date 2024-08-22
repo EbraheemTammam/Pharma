e1bcaee9544d00da90256130039f3ca70ac109b5
Ebraheem <ebraheem20012020@gmail.com>
2024-08-22 20:42:19 +0300

    redundant fields

--- Core/Modules/Users/Models/CustomUser.cs
+++ Core/Modules/Users/Models/CustomUser.cs
@@ -7,7 +7,5 @@ public class CustomUser : IdentityUser<int>
 {
     public required string FirstName {get; set;}
     public required string LastName {get; set;}
-    public required new string Email {get; set;}
-    public required string Password {get; set;}
     public bool IsAdmin {get; set;}
 }
