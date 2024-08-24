81596a9d6bf965c118d8592f3684cd4effed2d6a
Ebraheem <ebraheem20012020@gmail.com>
2024-08-24 22:50:06 +0300

    handle errors

--- API/Modules/Auth/Controllers/UsersController.cs
+++ API/Modules/Auth/Controllers/UsersController.cs
@@ -14,16 +14,25 @@ public class UsersController : GenericController<int, UserDTO>
     public UsersController(IAuthService authService) : base(authService) {}
 
     [HttpPost]
-    public async Task<IActionResult> Create(UserCreateDTO product) =>
-        Ok(
-            (await ((IAuthService)_service).Create(product)).GetResult<UserDTO>()
+    public async Task<IActionResult> Create(UserCreateDTO product)
+    {
+        BaseResponse result = await ((IAuthService)_service).Create(product);
+        return
+        (
+            result.StatusCode == 200 ? Ok(result.GetResult<UserDTO>())
+            : ProcessError(result)
         );
+    }
 
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(int id, UserCreateDTO product)
     {
-        BaseResponse response = await ((IAuthService)_service).Update(id, product);
-        if(response.StatusCode != 200) return ProcessError(response);
-        return Ok(response.GetResult<UserDTO>());
+        BaseResponse result = await ((IAuthService)_service).Update(id, product);
+        if(result.StatusCode != 200) return ProcessError(result);
+        return
+        (
+            result.StatusCode == 200 ? Ok(result.GetResult<UserDTO>())
+            : ProcessError(result)
+        );
     }
 }
