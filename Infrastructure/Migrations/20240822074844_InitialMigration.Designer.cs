ffbd3839668d6b9b4e86101c86dc3c26a8707ddb
Ebraheem <ebraheem20012020@gmail.com>
2024-08-26 22:22:32 +0300

    correcting using statements

--- Infrastructure/Migrations/20240822074844_InitialMigration.Designer.cs
+++ Infrastructure/Migrations/20240822074844_InitialMigration.Designer.cs
@@ -3,9 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using Pharmacy.Infrastructure.Generics;
+using Pharmacy.Infrastructure.Data;
 
 #nullable disable
 
