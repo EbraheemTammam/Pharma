f7fe351f529b9b1eccb25a303ca07577af163eaa
Ebraheem <ebraheem20012020@gmail.com>
2024-08-26 14:51:25 +0300

    change datatype to match database

--- Core/Modules/Orders/Models/Payment.cs
+++ Core/Modules/Orders/Models/Payment.cs
@@ -5,7 +5,7 @@ namespace Pharmacy.Domain.Modules.Orders.Models;
 
 public sealed class Payment : BaseModel<int>
 {
-    public decimal Paid {get; set;} = 0;
+    public double Paid {get; set;} = 0;
     public DateTime CreatedAt {get;} = DateTime.UtcNow;
     public required Guid CustomerId {get; set;}
 }
