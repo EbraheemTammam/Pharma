b0d0ceef2ca6263c8774b0eaf52a2160af7dfe14
Ebraheem <ebraheem20012020@gmail.com>
2024-08-26 20:52:51 +0300

    rename custom user to user

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 namespace Pharmacy.Domain.Models;
 
 
-public sealed class CustomUser : IdentityUser<int>
+public sealed class User : IdentityUser<int>
 {
     public required string FirstName {get; set;}
     public required string LastName {get; set;}
