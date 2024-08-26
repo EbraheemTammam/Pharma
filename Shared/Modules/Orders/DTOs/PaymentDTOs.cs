b57ebc3b700a0424eb0063ef393bc755949e48b8
Ebraheem <ebraheem20012020@gmail.com>
2024-08-26 14:51:53 +0300

    payment DTOs

---
+++ Shared/Modules/Orders/DTOs/PaymentDTOs.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy.Shared.Modules.Orders.DTOs;
+
+
+public class PaymentBaseDTO
+{
+    [Required]
+    public decimal AmountPaid {get; set;}
+}
+
+public class PaymentCreateDTO : PaymentBaseDTO
+{
+
+}
+
+public class PaymentDTO : PaymentBaseDTO
+{
+    public int Id {get; set;}
+    public DateTime CreatedAt {get; set;}
+}
