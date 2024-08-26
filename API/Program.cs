b4e75a5e5d1b3118c3cbfbe9ed54a3a515ff5a86
Ebraheem <ebraheem20012020@gmail.com>
2024-08-26 22:29:12 +0300

    app settings and configuring user secrets

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,11 @@
-using Pharmacy.Application.Utilities;
 using Pharmacy.Presentation.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if(builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
