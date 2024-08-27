7be646cdffcb627a222df2c8afd3d476d48740ad
Ebraheem <ebraheem20012020@gmail.com>
2024-08-27 18:23:10 +0300

    to model method in order mapper

--- Application/Mappers/OrderMapper.cs
+++ Application/Mappers/OrderMapper.cs
@@ -7,13 +7,25 @@ namespace Pharmacy.Application.Mappers;
 
 public static class OrderMapper
 {
+    public static Order ToModel(this OrderCreateDTO orderDTO, int userId) =>
+        new()
+        {
+            Paid = (double)orderDTO.Paid,
+            TotalPrice = 0,
+            UserId = userId,
+            CustomerId = orderDTO.CustomerId
+        };
+
     public static OrderDTO ToDTO(this Order order) =>
+        order.ToDTO(order.Customer!.Name, $"{order.CreatedBy!.FirstName} {order.CreatedBy.LastName}");
+
+    public static OrderDTO ToDTO(this Order order, string CustomerName, string userFullName) =>
         new()
         {
             Paid = (decimal)order.Paid,
             CreatedAt = order.CreatedAt,
             TotalPrice = (decimal)order.TotalPrice,
-            CustomerName = order.Customer!.Name,
-            CreatedBy = $"{order.CreatedBy!.FirstName} {order.CreatedBy.LastName}"
+            CustomerName = CustomerName,
+            CreatedBy = userFullName
         };
 }
