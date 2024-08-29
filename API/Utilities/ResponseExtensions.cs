d782b2bec92f6715fec39d9f1962d2ee284ba2de
Ebraheem <ebraheem20012020@gmail.com>
2024-08-29 10:11:27 +0300

    extract data from created response

--- API/Utilities/ResponseExtensions.cs
+++ API/Utilities/ResponseExtensions.cs
@@ -7,4 +7,6 @@ public static class ResponseExtensions
 {
     public static TResultType GetResult<TResultType>(this BaseResponse response) =>
         ((OkResponse<TResultType>)response).Result;
+    public static TResultType GetData<TResultType>(this BaseResponse response) =>
+        ((CreatedResponse<TResultType>)response).Data;
 }
