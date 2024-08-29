92b9894580f72ba19ac2f6bac82d17fc953583bf
Ebraheem <ebraheem20012020@gmail.com>
2024-08-29 10:10:58 +0300

    make property non nullable

--- Shared/Responses/CreatedResponse.cs
+++ Shared/Responses/CreatedResponse.cs
@@ -4,6 +4,6 @@ namespace Pharmacy.Shared.Responses;
 
 public class CreatedResponse<T> : BaseResponse
 {
-    public T? Data { get; set;}
+    public T Data { get; set;}
     public CreatedResponse(T data) : base(201) => Data = data;
 }
